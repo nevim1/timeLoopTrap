5ed743cb81e9c65f041c2179808842f4ec0ad767
nevim <ondrej.lupisek@gmail.com>
2025-07-30 23:16:00 +0200

    first steps

--- Player.cs
+++ Player.cs
@@ -1,6 +1,22 @@
 using Godot;
 using System;
 
-public partial class Player : CharacterBody2D
-{
+public partial class Player : CharacterBody2D {
+	[Export]
+	public int gridSize {get; set;} = 32;
+	private RayCast2D raycast;
+
+	public override void _Ready(){
+		raycast = GetNode<RayCast2D>("RayCast2D");
+	}
+
+	public override void _Input(InputEvent @event){
+		if Input.isActionPressed("moveUp"){
+			private Vector2D direction = gridSize * Vector2D.UP;
+			raycast.ForceRaycastUpdate();
+			if(!raycast.IsColliding()){
+				Position += direction;
+			}
+		}
+	}
 }
