7cdf6f835b9dcc6780a9cbb1af31221a42e985b9
VeejayIam <vitkosaurus@gmail.com>
2025-08-01 19:29:12 +0200

    added level select and ingame quit buttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/wrold.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels_menu.tscn")
 
 
 
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var stepLabel = $Steps
+@onready var stepLabel = $BoxContainer/Steps
 
 func update_steps(value):
 	stepLabel.text = 'Steps left: ' + str(value)
