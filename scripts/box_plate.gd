44327ce9ce80ce459ad22a8e3b0abf9d9724579f
nevim <ondrej.lupisek@gmail.com>
2025-08-02 17:51:10 +0200

    nothibg significant

--- scripts/box_plate.gd
+++ scripts/box_plate.gd
@@ -1,13 +1,14 @@
 extends Area2D
 
 func _on_area_exited(_body):
-	#get_collision_layer_value()
+	print(get_collision_layer_value(3))
 	print(_body)
 	for i in get_overlapping_areas():
 		if 'state_change' in i:
 			i.state_change(false)
 
 func _on_area_entered(_body):
+	print(get_collision_layer_value(3))
 	print(_body)
 	for i in get_overlapping_areas():
 		if 'state_change' in i:
