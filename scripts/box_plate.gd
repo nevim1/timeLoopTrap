1fa6faf0252da219f987ede58e168749b3be34ea
nevim <ondrej.lupisek@gmail.com>
2025-08-02 18:01:53 +0200

    minor changes

--- scripts/box_plate.gd
+++ scripts/box_plate.gd
@@ -8,7 +8,7 @@ func _on_area_exited(_body):
 			i.state_change(false)
 
 func _on_area_entered(_body):
-	print(get_collision_layer_value(3))
+	print(_body.get_collision_layer())
 	print(_body)
 	for i in get_overlapping_areas():
 		if 'state_change' in i:
