3966214cf872056619a730e89557522d9bdc3815
nevim <ondrej.lupisek@gmail.com>
2025-08-03 11:57:05 +0200

    undoing now works for buttons

--- scripts/button.gd
+++ scripts/button.gd
@@ -7,14 +7,20 @@ extends Area2D
 
 var activated : bool = false
 
+var activation_step : int
+var sim_step : int = 0
+
 func _ready() -> void:
 	level.end_loop.connect(end_loop)
 	level.reset_loop.connect(reset_loop)
+	level.undo.connect(undo)
+	level.step.connect(step)
 	animation_player.set_frame(0)
 
 func _on_body_entered(_body):
 	if not activated:
 		activated = true
+		activation_step = sim_step
 		for i in get_overlapping_areas():
 			if 'state_change' in i:
 				i.state_change(true)
@@ -24,6 +30,19 @@ func _on_body_entered(_body):
 func end_loop():
 	activated = false
 	animation_player.set_frame(0)
+	sim_step = 0
 
 func reset_loop():
 	end_loop()
+
+func step():
+	sim_step += 1
+	
+func undo():
+	sim_step -= 1
+	if sim_step == activation_step-1:
+		activated = false
+		animation_player.set_frame(0)
+		for i in get_overlapping_areas():
+			if 'state_change' in i:
+				i.state_change(false)
