71884abecb6c4710c3a811c34f983d05d0366cc3
VeejayIam <vitkosaurus@gmail.com>
2025-10-02 11:22:28 +0200

    named layer 5 and added conveyor tile(not functional)

---
+++ scripts/conveyor.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_area_exited(area: Area2D) -> void:
+	pass # Replace with function body.
