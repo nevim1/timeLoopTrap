4afaf938485b2c6de9bcd6443f3c1d64c724f909
nevim <ondrej.lupisek@gmail.com>
2025-10-14 11:39:09 +0200

    reamed some tiles with properties, made  finish line (stub)

--- scripts/tile_properties/finish.gd
+++ scripts/tile_properties/finish.gd
@@ -1,6 +1,20 @@
 extends Module
 
-
+var level : Node2D
 
 func _ready():
-	print(parent)
+	if root.find_child('level'):
+		level = root.get_node('level')
+		print(level)
+
+func win(_trash):
+	var level_name = level.scene_file_path
+	var regex = RegEx.new()
+	regex.compile(r"level_(\d+)\.tscn")
+	var result = regex.search(level_name)
+	var levelnumber = result.get_string(1)
+	var next_level_path = "res://scenes/levels/level_%d.tscn" % (int(levelnumber) + 1)
+	if ResourceLoader.exists(next_level_path):
+		get_tree().call_deferred('change_scene_to_file', next_level_path)
+		return
+	get_tree().call_deferred('change_scene_to_file', "res://scenes/ui/levels_menu.tscn")
--- scripts/tile_properties/module.gd
+++ scripts/tile_properties/module.gd
@@ -1,3 +1,4 @@
 class_name Module extends Node
 
 @onready var parent = get_parent()
+@onready var root = get_tree().get_root()
