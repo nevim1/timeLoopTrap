0f4047ac0deb97c330afde2b5b03443977f624b7
VeejayIam <vitkosaurus@gmail.com>
2025-11-03 15:17:43 +0100

    moved wire logic to signal reciever and fixed error with spawner

--- scripts/tile_properties/player_spawner.gd
+++ scripts/tile_properties/player_spawner.gd
@@ -1,1 +1,18 @@
 extends Module
+
+@onready var level : Node2D = get_tree().get_root().get_node('level')
+@onready var tile_set : TileMapLayer = level.get_node('BGTileMapLayer')
+@onready var animation_player : AnimatedSprite2D = get_node("../AnimatedSprite2D")
+@onready var ray_cast_2d : RayCast2D = get_node("../BoxRaycast") 
+
+var player_scene : PackedScene = preload('res://scenes/tiles/Player.tscn')
+
+
+func _ready():
+	animation_player.play("default_1")
+	level.end_loop.connect(end_loop)
+
+func end_loop():
+	var new_player = player_scene.instantiate()
+	new_player.position = get_parent().position + Vector2(32,0)
+	tile_set.add_child(new_player)
--- scripts/tile_properties/signal_reciever.gd
+++ scripts/tile_properties/signal_reciever.gd
@@ -1,1 +1,32 @@
 extends Module
+
+@onready var animation_player = get_node("../Sprite2D") 
+
+@onready var level : Node2D = get_tree().get_root().get_node('level')
+
+var signalState : bool = false
+
+func _ready() -> void:
+	level.end_loop.connect(end_loop)
+	level.reset_loop.connect(reset_loop)
+	signalState = false
+
+func state_change(state : bool):
+	if signalState != state:
+		signalState = state
+		for i in get_overlapping_areas():
+			if 'state_change' in i:
+				i.state_change(state)
+
+		if signalState:
+			animation_player.set_frame(1)
+		else:
+			animation_player.set_frame(0)
+		
+
+func end_loop():
+	animation_player.set_frame(0)
+	signalState = false
+
+func reset_loop():
+	end_loop()
